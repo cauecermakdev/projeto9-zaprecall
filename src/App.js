099f119767d335c66ff827871ca0d8ade9799b29
Cauê Pereira Cermak <cauecermak@gmail.com>
2022-09-27 13:33:37 +0000

    inicializando

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./globalStyles";
+
+export default function App(){
+
+    return (
+        <>
+            <GlobalStyle />
+            <p>quaquer coisa</p> 
+        </>
+    )
+
+}
---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    /*Aqui vai meu CSS*/
+
+    body{
+        
+    }
+`;
+
+export default GlobalStyle;
